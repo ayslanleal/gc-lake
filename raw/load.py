b3281b8f083f12835efa72af63b5bbc3216ab000
ayslanleal <ayslanleal15@gmail.com>
2022-05-20 19:14:04 -0300

    Add batch s3

--- raw/load.py
+++ raw/load.py
@@ -6,7 +6,7 @@ import sqlalchemy
 
 def save_bucket_s3(db, table, s3_client):
     df = pd.read_sql(table, db)
-    df.to_csv(f"../data/{table}.csv")
+    df.to_csv(f"../data/{table}.csv", index=False)
     s3_client.upload_file(Bucket='gc-datalke-delta', Filename=f"../data/{table}.csv", Key=f'raw/gc/full_load/{table}.csv')
     return True
 
